e05341107dd66cdab8b321df608d0ca65b581911
aRigon <andrea.rigon_03@studenti.univr.it>
2025-07-19 12:11:53 +0200

    frontend fixed

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,27 +1,24 @@
-import { IoStatsChartSharp ,IoClose} from "react-icons/io5";
-import '../styles.css'
-import { Dialog, DialogPanel} from "@headlessui/react";
+import { IoStatsChartSharp, IoClose } from "react-icons/io5";
+import "../styles.css";
+import { Dialog, DialogPanel } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import useStatusStore from "../store/statusStore";
 
-
 function Header() {
-   
-  const [open,setOpen] = useState(false);
-  const {win,played,gameHistory} = useStatusStore();
-  const [maxStreak,setMaxStreak] = useState(0);
-  const [currentStreak,setCurrentStreak] = useState(0);
+  const [open, setOpen] = useState(false);
+  const { win, played, gameHistory } = useStatusStore();
+  const [maxStreak, setMaxStreak] = useState(0);
+  const [currentStreak, setCurrentStreak] = useState(0);
 
-  const close = ()=>
-  {
+  const close = () => {
     setOpen(false);
-  }
+  };
 
   const getStreaksInfo = () => {
     let appo = [];
     let count = 0;
     let dim = gameHistory.length;
-  
+
     for (let i = 0; i < dim; i++) {
       if (gameHistory[i] === true) {
         count++;
@@ -32,11 +29,11 @@ function Header() {
         }
       }
     }
-  
+
     if (count > 0) appo.push(count);
-  
+
     setMaxStreak(appo.length > 0 ? Math.max(...appo) : 0);
-  
+
     let current = 0;
     for (let i = dim - 1; i >= 0; i--) {
       if (gameHistory[i] === true) {
@@ -47,64 +44,88 @@ function Header() {
     }
     setCurrentStreak(current);
   };
-  
-  
-  useEffect (()=>
-  {
-      getStreaksInfo();
-  },[gameHistory]);
+
+  useEffect(() => {
+    getStreaksInfo();
+  }, [gameHistory]);
 
   return (
-    <div className='flex flex-row justify-between  py-5 items-baseline w-full gap-2 h-1/20'>
-            <p className='text-bold text-lg text-gray-400'>casuale</p>
-            <p className='text-bold  text-5xl'>WORDLE</p>
-            <button onClick={(e)=>{
-              setOpen(true);
-              e.currentTarget.blur();
-            }}>
-              <IoStatsChartSharp id="stats" size={30} className="cursor-pointer"/>
-            </button>
-            <Dialog open={open} as="div" className="relative z-10 focus:outline-none" onClose={close}>
-                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                          <div className="flex min-h-full items-center justify-center p-4">
-                            <DialogPanel
-                              transition
-                              className="border-2 border-black w-full max-w-md h-full  flex flex-col justify-center items-center rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
-                            >
-                              <div className="justify-between flex flex-row w-full">
-                                <p></p>
-                                <IoClose className=" font-bold" size={24} onClick={()=>close()}/>
+    <>
+      {open ? (
+        <Dialog
+          open={open}
+          as="div"
+          className="relative z-10 focus:outline-none"
+          onClose={close}
+        >
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4">
+              <DialogPanel
+                transition
+                className="border-2 border-black w-full max-w-md h-full  flex flex-col justify-center items-center rounded-xl bg-white p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0"
+              >
+                <div className="justify-between flex flex-row w-full">
+                  <p></p>
+                  <IoClose
+                    className=" font-bold"
+                    size={24}
+                    onClick={() => close()}
+                  />
+                </div>
 
-                              </div>
-
-                               <h1 className="font-bold pb-5">Queste sono le tue statistiche</h1>
-                               <div className="flex flex-row gap-4">
-                                 <div className="flex flex-col">
-                                       <p className="flex justify-center items-center">{played}</p>
-                                       <p className="flex justify-center items-center">Partite</p>
-                                       <p className="flex justify-center items-center">Giocate</p>
-                                 </div>
-                                 <div className="flex flex-col">
-                                       <p className="flex justify-center items-center">{win}</p>
-                                       <p>Vittorie</p>
-                                 </div>
-                                 <div className="flex flex-col">
-                                       <p className="flex justify-center items-center">{currentStreak}</p>
-                                       <p className="flex justify-center items-center">Current</p>
-                                       <p className="flex justify-center items-center">Streak</p>
-                                 </div>
-                                 <div className="flex flex-col">
-                                      <p className="flex justify-center items-center">{maxStreak}</p>
-                                       <p className="flex justify-center items-center">Max</p>
-                                       <p>Streak</p>
-                                 </div>
-                               </div>
-                            </DialogPanel>
-                          </div>
-                        </div>
-                      </Dialog>
-         </div>
-  )
+                <h1 className="font-bold pb-5">
+                  Queste sono le tue statistiche
+                </h1>
+                <div className="flex flex-row gap-4">
+                  <div className="flex flex-col">
+                    <p className="flex justify-center items-center">{played}</p>
+                    <p className="flex justify-center items-center">Partite</p>
+                    <p className="flex justify-center items-center">Giocate</p>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="flex justify-center items-center">{win}</p>
+                    <p>Vittorie</p>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="flex justify-center items-center">
+                      {currentStreak}
+                    </p>
+                    <p className="flex justify-center items-center">Current</p>
+                    <p className="flex justify-center items-center">Streak</p>
+                  </div>
+                  <div className="flex flex-col">
+                    <p className="flex justify-center items-center">
+                      {maxStreak}
+                    </p>
+                    <p className="flex justify-center items-center">Max</p>
+                    <p>Streak</p>
+                  </div>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </Dialog>
+      ) : (
+        <div></div>
+      )}
+      <div className="flex flex-row w-full h-1/2 pt-3 items-baseline  justify-between px-5 pb-4">
+        <p className="text-bold text-lg text-gray-400 ">casuale</p>
+        <p className="text-bold  text-4xl ">WORDLE</p>
+        <button
+          onClick={(e) => {
+            setOpen(true);
+            e.currentTarget.blur();
+          }}
+        >
+          <IoStatsChartSharp
+            id="stats"
+            size={30}
+            className="cursor-pointer  self-baseline"
+          />
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Header;
--- src/components/keyboard.tsx
+++ src/components/keyboard.tsx
@@ -1,97 +1,79 @@
-import clsx from 'clsx';
-import '../styles.css'
-import useStatusStore from '../store/statusStore';
-import { useEffect } from 'react';
+import clsx from "clsx";
+import "../styles.css";
+import useStatusStore from "../store/statusStore";
+import { useEffect } from "react";
 
-export default function Keyboard({setPhrase}:{setPhrase : (ch:string)=>void}) {
-    
-    const test = 0;   
+export default function Keyboard({
+  setPhrase,
+}: {
+  setPhrase: (ch: string) => void;
+}) {
+  const test = 0;
 
-    const qwertyKeys = [
-        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-        ['ENTER','z', 'x', 'c', 'v', 'b', 'n', 'm','⌫']
-      ];
-    
-      const onClick =(ch: string) => {  
-         setPhrase(ch)
-      };
+  const qwertyKeys = [
+    ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+    ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+    ["ENTER", "z", "x", "c", "v", "b", "n", "m", "⌫"],
+  ];
 
-      const {grayLetters,greenLetters,yellowLetters} = useStatusStore();
-      
-      const getColor = (ch:string)=>
-      {
-          if (greenLetters.includes(ch))
-          {
-            return "bg-[#6ca965] text-white";
-          }
-          else if (yellowLetters.includes(ch))
-          {
-            return "bg-[#c8b653] text-white";
-          }
-          else if (grayLetters.includes(ch))
-          {
-              return "bg-[#787c7f] text-white";
-          }
-          else
-          {
-            return "bg-gray-200 text-black";
-          }
-      }
-      
-      const keyDownHandler =(event:any)=>
-      {
-          if (event.key === "Enter")
-          {
-            setPhrase('ENTER');
-          }
-          else if(event.key === "Backspace")
-          {
-            setPhrase('⌫');
-          }
-          else if ( event.key >= "a" && event.key<="z")
-          {
-            setPhrase(event.key);
-          }
-          else{
+  const onClick = (ch: string) => {
+    setPhrase(ch);
+  };
 
-            console.log("Character not allowed");
-          }
-          
-      };
+  const { grayLetters, greenLetters, yellowLetters } = useStatusStore();
 
+  const getColor = (ch: string) => {
+    if (greenLetters.includes(ch)) {
+      return "bg-[#6ca965] text-white hover:bg-green-700";
+    } else if (yellowLetters.includes(ch)) {
+      return "bg-[#c8b653] text-white hover:bg-yellow-400";
+    } else if (grayLetters.includes(ch)) {
+      return "bg-[#787c7f] text-white hover:bg-gray-500 ";
+    } else {
+      return "bg-gray-200 text-black hover:bg-gray-300 active:bg-gray-300 ";
+    }
+  };
 
-      useEffect(() => {
-          document.addEventListener('keydown',keyDownHandler)
-          
-          return ()=>{
-            document.removeEventListener('keydown',keyDownHandler)
-          }
+  const keyDownHandler = (event: any) => {
+    if (event.key === "Enter") {
+      setPhrase("ENTER");
+    } else if (event.key === "Backspace") {
+      setPhrase("⌫");
+    } else if (event.key >= "a" && event.key <= "z") {
+      setPhrase(event.key);
+    } else {
+      console.log("Character not allowed");
+    }
+  };
 
-      }, []);
+  useEffect(() => {
+    document.addEventListener("keydown", keyDownHandler);
 
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, []);
 
   return (
-  <div className="flex flex-col items-center justify-between h-3/5 w-full pt-6"> 
-    <div className="flex flex-col items-center px-2 pb-24 gap-1 w-full max-w-sm mx-auto">
-      {qwertyKeys.map((row, index) => (
-        <div key={index} className="flex gap-2 justify-center w-full">
-          {row.map((letter, number) => (
-            <button
-              onClick={() => onClick(letter)}
-              key={number}
-              className={clsx(
-                "flex items-center justify-center h-14 w-14 sm:h-14 sm:w-15 px-1 py-1 sm:px-2 sm:py-2 font-bold rounded-sm text-sm sm:text-base cursor-pointer",
-                getColor(letter)
-              )}
-            >
-              <p className="flex font-bold">{letter}</p>
-            </button>
-          ))}
-        </div>
-      ))}
+    <div className="flex flex-col items-center justify-between h-3/5 w-full pt-6">
+      <div className="flex flex-col items-center px-2 pb-24 gap-1 w-full max-w-sm mx-auto ">
+        {qwertyKeys.map((row, index) => (
+          <div key={index} className="flex gap-2 justify-center w-full">
+            {row.map((letter, number) => (
+              <button
+                onClick={() => onClick(letter)}
+                key={number}
+                className={clsx(
+                  "flex items-center justify-center  h-14 w-14 sm:h-14 sm:w-15 px-1 py-1 sm:px-2 sm:py-2 font-bold rounded-sm text-sm sm:text-base cursor-pointer ",
+                  getColor(letter)
+                )}
+              >
+                <p className="flex font-bold ">{letter}</p>
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-
+  );
 }
